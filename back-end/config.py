58cf464801ddc7e463fda3fd60ec8157fa3a0250
World <World>
2019-08-12 19:03:48 +0800

    standard code

--- back-end/config.py
+++ back-end/config.py
@@ -1,10 +1,11 @@
-APPID = "wx7376a2b8c3146546"
-APP_SECRTET = "877c99113297bb5fd8e41e9e9b831dff"
-MCH_ID = "1534364961"
-MCH_KEY = "wieuro9874297352sryoqiwUIUEUYwie"
+# 以下均为示范值，请自行替换
+WEIXIN_APP_ID = "wx7376a2b8c3146546"
+WEIXIN_APP_SECRET = "877c99113297bb5fd8e41e9e9b831dff"
+WEIXIN_MCH_ID = "1534364961"
+WEIXIN_MCH_KEY = "wieuro9874297352sryoqiwUIUEUYwie"
+WEIXIN_NOTIFY_URL = BASE_URL + "/wxpay/notify"
 
 # url处理
-encodeURL = "http%3A%2F%2Fwww.ai-solution.cn%2Fodoo%2Fauthorized"  # 网站域名的encodeURLComponent
-URL = "http://www.ai-solution.cn/odoo/activity/"
-base_url = "http://www.ai-solution.cn/odoo"
-notify_url = base_url + "/wxpay/notify"  # 异步接收微信支付结果通知的回调地址，通知url必须为外网可访问的url，不能携带参数
+ENCODE_URL = "http%3A%2F%2Fwww.ai-solution.cn%2Fodoo%2Fauthorized"  # 网站域名的encodeURLComponent
+# URL = "http://www.ai-solution.cn/odoo/activity/"
+BASE_URL = "http://www.ai-solution.cn/odoo"
--- back-end/wxpay.py
+++ back-end/wxpay.py
@@ -27,14 +27,14 @@ class WxPay(http.Controller):
         self.mch_key = config.MCH_KEY
 
         # url处理
-        self.encodeURL = config.encodeURL
-        self.URL = config.URL
-        self.base_url = config.base_url
-        self.notify_url = config.notify_url
+        self.encodeURL = config.ENCODE_URL
+        # self.URL = config.URL
+        self.base_url = config.BASE_URL
+        self.notify_url = config.WEIXIN_NOTIFY_URL
 
         # 初始化
-        wx_login = WeixinLogin(self.app_id, self.app_secret)
-        wx_pay = WeixinPay(self.app_id, self.mch_id, self.mch_key, self.notify_url)
+        self.wx_login = WeixinLogin(self.app_id, self.app_secret)
+        self.wx_pay = WeixinPay(self.app_id, self.mch_id, self.mch_key, self.notify_url)
     @http.route("/odoo/login/<param>", auth="public", website=True)
     def login(self, param, **kwargs):
         """
@@ -42,14 +42,14 @@ class WxPay(http.Controller):
         :param param: 传入参数名（非字典、列表）
         :return:
         """
-        redirect_url = self.URL + param
+        redirect_url = self.base_url + param
         openid = request.httprequest.cookies.get("openid")
         next = request.httprequest.args.get("next") or redirect_url
         if openid:
             return request.redirect(next)
         # callback = url_for("authorized", next=next, _external=True)
         callback = self.encodeURL + "?next=" + next
-        url = wx_login.authorize(callback, "snsapi_base")  # 引导用户跳转到授权页面，callback = redirect_uri
+        url = self.wx_login.authorize(callback, "snsapi_base")  # 引导用户跳转到授权页面，callback = redirect_uri
         return request.redirect(url)
 
     @http.route("/odoo/authorized", auth="public", website=True)
@@ -62,7 +62,7 @@ class WxPay(http.Controller):
         if not code:
             return "ERR_INVALID_CODE", 400
         next = request.httprequest.args.get("next")
-        data = wx_login.access_token(code)  # 通过code换取网页授权access_token
+        data = self.wx_login.access_token(code)  # 通过code换取网页授权access_token
         openid = data.openid
         resp = request.redirect(next)
         expires = datetime.now() + timedelta(days=1)
@@ -83,8 +83,8 @@ class WxPay(http.Controller):
         try:
             remote_addr = request.httprequest.environ['REMOTE_ADDR']  # 获取远程客户端IP
             openid = request.httprequest.cookies.get("openid")
-            out_trade_no = wx_pay.nonce_str  # 商户订单号设定为随机字符串
-            raw = wx_pay.jsapi(openid=openid, body=u"测试", out_trade_no=out_trade_no, total_fee=1, spbill_create_ip=remote_addr)  # 外部传入openid
+            out_trade_no = self.wx_pay.nonce_str  # 商户订单号设定为随机字符串
+            raw = self.wx_pay.jsapi(openid=openid, body=u"测试", out_trade_no=out_trade_no, total_fee=1, spbill_create_ip=remote_addr)  # 外部传入openid
             return json.dumps(raw)  # 直接返回包含Json格式数据响应的方法
         except WeixinPayError as e:
             print(e.message)
@@ -95,7 +95,7 @@ class WxPay(http.Controller):
         """
         微信异步通知
         """
-        data = wx_pay.to_dict(request.data)
-        if not wx_pay.check(data):
-            return wx_pay.reply("签名验证失败", False)
-        return wx_pay.reply("OK", True)  # 处理业务逻辑
+        data = self.wx_pay.to_dict(request.data)
+        if not self.wx_pay.check(data):
+            return self.wx_pay.reply("签名验证失败", False)
+        return self.wx_pay.reply("OK", True)  # 处理业务逻辑
